770f39517591fe5bf22697f05aed090182e665d1
Mark Connell <mark@markconnell.co.uk>
2010-07-02 00:13:51 +0100

    Bus Stops have codes.

--- test/bus_tracker_test.rb
+++ test/bus_tracker_test.rb
@@ -9,7 +9,8 @@ class BusTrackerTest < ActiveSupport::TestCase
     testee.bus_stops.each do |bus_stop|
       assert(bus_stop.is_a?(BusTracker::BusStop), "bus stop isn't a BusTracler::BusStop instance")
       assert(bus_stop.service_numbers.include?(testee.number), "Service numbers do not include #{testee.number}")
-      assert_equal(bus_stop.name.class, String)
+      assert_equal(String, bus_stop.name.class)
+      assert_equal(String, bus_stop.code.class)
     end
   end
 
