a9becb37903f642fb2a8255102f22747b46cf439
Mark Connell <mark@markconnell.co.uk>
2010-07-01 22:11:13 +0100

    Helps if I put lat/long the right way around..

--- lib/bus_tracker.rb
+++ lib/bus_tracker.rb
@@ -1,6 +1,9 @@
 require 'nokogiri'
 require 'open-uri'
 
+require 'bus_tracker/service'
+require 'bus_tracker/bus_stop'
+
 module BusTracker
 
   BASE_URI = 'http://mybustracker.co.uk/'
@@ -12,8 +15,8 @@ module BusTracker
     bus_stops = document.xpath('//map/markers/busstop')
     bus_stops.each do |bus_stop|
       name             = bus_stop.xpath('nom')[0].content
-      longitude        = bus_stop.xpath('x')[0].content
-      latitude         = bus_stop.xpath('y')[0].content
+      latitude         = bus_stop.xpath('x')[0].content
+      longitude        = bus_stop.xpath('y')[0].content
       service_numbers  = bus_stop.xpath('services/service/mnemo').map {|s| s.content}
 
       service.bus_stops << BusTracker::BusStop.new(
