38c6284655dea384de0ee471ddbcaec1c7a8a086
Mark Connell <mark@markconnell.co.uk>
2010-07-02 00:08:00 +0100

    Don't talk to the bustracker service during tests, fake out the connection with a cached xml sample.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,15 @@
 require 'rubygems'
 require 'test/unit'
 require 'active_support'
+require 'fakeweb'
+FakeWeb.allow_net_connect = false
 
 require 'nokogiri'
 require 'bus_tracker'
 require 'bus_tracker/service'
 require 'bus_tracker/bus_stop'
+
+# Fake out the fetching of data for a bus service
+FakeWeb.register_uri :get,
+  'http://mybustracker.co.uk/getServicePoints.php?serviceMnemo=30',
+  :body => File.open('/Users/mark/workspaces/ruby/bustracker/test/fake_service_number_response.xml', 'rb').read
