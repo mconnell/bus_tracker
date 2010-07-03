f7e02ec655dab92c7b29ce07a9245e999aca77d3
Mark Connell <mark@markconnell.co.uk>
2010-07-03 13:05:45 +0100

    Tidy up the creation of new bus stops, don't be overeager with local variables.

--- lib/bus_tracker/service.rb
+++ lib/bus_tracker/service.rb
@@ -13,18 +13,12 @@ module BusTracker
       document  = Nokogiri::HTML(open("#{BASE_URI}getServicePoints.php?serviceMnemo=#{self.number}"))
       bus_stops = document.xpath('//map/markers/busstop')
       bus_stops.each do |bus_stop|
-        code             = bus_stop.xpath('sms')[0].content
-        name             = bus_stop.xpath('nom')[0].content
-        latitude         = bus_stop.xpath('x')[0].content
-        longitude        = bus_stop.xpath('y')[0].content
-        service_numbers  = bus_stop.xpath('services/service/mnemo').map {|s| s.content}
-
         self.bus_stops << BusTracker::BusStop.new(
-          :code      => code,
-          :name      => name,
-          :latitude  => latitude,
-          :longitude => longitude,
-          :service_numbers => service_numbers
+          :code            => bus_stop.xpath('sms')[0].content,
+          :name            => bus_stop.xpath('nom')[0].content,
+          :latitude        => bus_stop.xpath('x')[0].content,
+          :longitude       => bus_stop.xpath('y')[0].content,
+          :service_numbers => bus_stop.xpath('services/service/mnemo').map {|s| s.content}
         )
       end
     end
