ba892e9cf7ef2271592e62aa874ccdeb5a8d2f99
Mark Connell <mark@markconnell.co.uk>
2010-07-03 12:57:26 +0100

    Refactor the BusTracker.service method.

--- lib/bus_tracker.rb
+++ lib/bus_tracker.rb
@@ -10,26 +10,8 @@ module BusTracker
   BASE_URI = 'http://mybustracker.co.uk/'
 
   def self.service(number)
-    service = BusTracker::Service.new(:number => number)
-
-    document  = Nokogiri::HTML(open("#{BASE_URI}getServicePoints.php?serviceMnemo=#{number}"))
-    bus_stops = document.xpath('//map/markers/busstop')
-    bus_stops.each do |bus_stop|
-      code             = bus_stop.xpath('sms')[0].content
-      name             = bus_stop.xpath('nom')[0].content
-      latitude         = bus_stop.xpath('x')[0].content
-      longitude        = bus_stop.xpath('y')[0].content
-      service_numbers  = bus_stop.xpath('services/service/mnemo').map {|s| s.content}
-
-      service.bus_stops << BusTracker::BusStop.new(
-        :code      => code,
-        :name      => name,
-        :latitude  => latitude,
-        :longitude => longitude,
-        :service_numbers => service_numbers
-      )
-    end
-
+    service = BusTracker::Service.new(number)
+    service.fetch_bus_stops!
     service
   end
 
--- test/bus_tracker/service_test.rb
+++ test/bus_tracker/service_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class BusTracker::ServiceTest < ActiveSupport::TestCase
 
   test "instanciating a new service should have an empty array of bus stops" do
-    testee = BusTracker::Service.new
+    testee = BusTracker::Service.new('30')
     assert(testee.bus_stops.is_a?(Array))
     assert_equal(0, testee.bus_stops.size)
   end
